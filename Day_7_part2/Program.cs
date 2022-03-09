d8f899c126d3b56e041e99e584a3cf50bb145dd5
DoraLiv <dora.livkisa@gmail.com>
2022-03-09 19:16:12 +0200

    Initial code

--- Day_7_part2/Program.cs
+++ Day_7_part2/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Please write the number of the day");
+int day = int.Parse(Console.ReadLine());
+
+switch (day)
+{
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("It is a working day");
+        break;
+    case 6:
+    case 7:
+        Console.WriteLine("It is holiday");
+        break;
+        default:
+        Console.WriteLine("Invalid number!");
+        break;
+}
